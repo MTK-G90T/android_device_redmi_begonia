761c02c81b9c8ff85cc5d95de083ff37e516f5b0
LinuxSupremacyX97 <87778892+LinuxSupremacyX97@users.noreply.github.com>
2023-09-27 00:53:57 +0530

    begonia: Add vendorsetup

---
+++ vendorsetup.sh
@@ -0,0 +1,26 @@
+# Kernel
+git clone https://github.com/Nova-Kernels/kernel_xiaomi_mt6785 kernel/xiaomi/mt6785
+
+# Vendor 
+git clone https://github.com/begonia-dev/android_vendor_redmi_begonia vendor/redmi/begonia
+
+# Hardware
+git clone https://github.com/begonia-dev/android_hardware_mediatek hardware/mediatek
+
+# Sepolicy
+git clone https://github.com/begonia-dev/android_device_mediatek_sepolicy device/mediatek/sepolicy
+
+# IMS
+git clone https://github.com/Trinity-ROMS/android_vendor_redmi_begonia-ims vendor/redmi/begonia-ims
+
+# Firmware
+git clone https://github.com/Trinity-ROMS/android_vendor_redmi_begonia-firmware vendor/redmi/begonia-firmware
+
+# MtkFMRadio
+git clone https://github.com/begonia-dev/android_packages_apps_MtkFMRadio packages/apps/MtkFMRadio
+
+# v4A
+git clone https://github.com/Bagualisson-Tests/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
+
+# ANX
+git clone https://github.com/shhhamik/vendor_ANXCamera vendor/ANXCamera
